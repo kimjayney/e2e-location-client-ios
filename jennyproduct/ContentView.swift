698161f6fbf7a3d60033bbb3e4c242557cefc600
Administrator <gim515@naver.com>
2020-07-26 20:45:15 +0900

    Initial Commit

---
+++ jennyproduct/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  jennyproduct
+//
+//  Created by jenny on 2020/07/26.
+//  Copyright © 2020 jenny. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
